78feaa0078e7accbb0610ff5224e7ba9fed29d09
HatemHK1 <64265896+HatemHK1@users.noreply.github.com>
2021-11-02 01:00:52 +0200

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,10 @@
+input ('enetr your name : ') user
+print ('Wellcome +user')
+from gtts import gTTS # pip install gTTS
+from playsound import playsound # pip install playsound
+i = input('ENTRE YOUR NAME :')
+x = 'hey '+i+ 'welcome to my tool, i hope you like it'
+la = 'en'
+g = gTTS(text=x,lang=la,slow=False)
+out = g.save('voice.mp3')
+playsound('voice.mp3')
